e082f0af917391872eda798ff0401a4f2512c3c5
chien <chien.buivan@hotmail.com>
2024-09-07 10:22:27 +0700

    Init project
    Signed-off-by: chien <chien.buivan@hotmail.com>

---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+#include <timers.h>
+#include <semphr.h>
+
+/* Standard includes. */
+#include <stdio.h>
+
+void main( void )
+{
+    /* Start the scheduler. */
+    vTaskStartScheduler();
+
+    for( ; ; )
+    {
+        /* Should not reach here. */
+    }
+}
+/*-----------------------------------------------------------*/
+
+#if ( configCHECK_FOR_STACK_OVERFLOW > 0 )
+
+    void vApplicationStackOverflowHook( TaskHandle_t xTask,
+                                        char * pcTaskName )
+    {
+        /* Check pcTaskName for the name of the offending task,
+         * or pxCurrentTCB if pcTaskName has itself been corrupted. */
+        ( void ) xTask;
+        ( void ) pcTaskName;
+    }
+
+#endif /* #if ( configCHECK_FOR_STACK_OVERFLOW > 0 ) */
+/*-----------------------------------------------------------*/
